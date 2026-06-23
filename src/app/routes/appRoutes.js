448ed76e89c5bfdd34b643e6291b6363d85f83bc
Ashwin Thomas <ashwint@skellam.ai>
2026-06-23 09:57:26 +0530

    dashboard-wiki

--- src/app/routes/appRoutes.js
+++ src/app/routes/appRoutes.js
@@ -11,7 +11,7 @@ const appRoute = () => {
   return (
     <>
       <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
+      <Route path="/wiki/customer-dashboard" element={<AboutPage />} />
     </>
   )
 }
